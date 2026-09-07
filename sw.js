f45951e107fc2bacb37c4e63b9b950211a4d079c
winnieisabiscuit <winnieisateapot@foxmail.com>
2026-09-07 11:37:12 +0800

    Add service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='daily-todo-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(hit=>hit||fetch(event.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return resp;}).catch(()=>caches.match('./index.html'))));});
